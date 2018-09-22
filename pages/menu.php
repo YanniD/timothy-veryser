01aaa8df990307e16713ade1dce03117d43cc61d
YanniD <yanni.derous@student.howest.be>
2018-09-22 21:33:50 +0200

    photo

--- pages/menu.php
+++ pages/menu.php
@@ -1,9 +1,8 @@
+<div class="row">
+  <div class="col col-lg">
+    <figure>
+      <img id='timothyMenu' class="img-fluid" src="/public/assets/timothy.jpg" alt="Timothy" title='Timothy'>
+    </figure>
 
-  <div class="row">
-    <div class="col col-lg-12">
-      <figure>
-        <img id='timothyMenu' class="img-fluid" src="/public/assets/timothy.jpg" alt="Timothy" title='Timothy'>
-      </figure>
-
-    </div>
   </div>
+</div>
