bc824b80f5821b1c64230990add6cc2f35de4a43
YanniD <yanni.derous@student.howest.be>
2018-09-22 14:26:00 +0200

    'setup'

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('pages/header.php');?>
+<?php include('pages/menu.php');?>
+<?php include('pages/over.php');?>
+<?php include('pages/videos.php');?>
+<?php include('pages/contact.php');?>
+<?php include('pages/footer.php');?>
+
+
