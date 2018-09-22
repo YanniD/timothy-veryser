752e38cb20104a999a8eb1c90755e223305a6283
Tibo <schellens.tibo@outlook.com>
2018-09-22 21:20:00 +0200

    footer and images

--- pages/footer.php
+++ pages/footer.php
@@ -1,48 +1,38 @@
 
-  <!-- Footer -->
   <footer class="page-footer font-small blue pt-4 footer-color">
 
-      <!-- Footer Links -->
       <div class="container-fluid text-center text-md-left">
 
-        <!-- Grid row -->
         <div class="row">
-
-          <!-- Grid column -->
-          <div class="col-md-6 mt-md-0 mt-3">
-
-            <!-- Content -->
-            <h5 class="text-uppercase">Footer Content</h5>
-            <p>Here you can use rows and columns here to organize your footer content.</p>
+          <div class="col-md-6 mt-md-0 mt-3 credit-baseline">
+            <p class="footer-text-color credit-height">design made by Tibo Schellens</p>
+            <p class="footer-text-color credit-height">programmed by yanni</p>
 
           </div>
 
             <!-- Grid column -->
-            <div class="col-md-right">
+            <div class="col-lg-right ml-auto link-margin">
 
               <!-- Links -->
-              <h5 class="text-uppercase">Social</h5>
+              <h5 class="text-uppercase footer-text-color">Social</h5>
 
-              <ul class="list-unstyled">
-                <li>
-                  <a class="footer-text-color" href="#!">Link 1</a>
+              <ul class="list-unstyled mr-auto">
+                <li class="link-top">
+                  <a class="footer-text-color" href="#!"><img class="image-size" src="/public/assets/facebook.png" alt="facebook"> @TimothyVeryserOpera</a>
                 </li>
-                <li>
-                  <a class="footer-text-color" href="#!">Link 2</a>
+                <li class="link-top">
+                  <a class="footer-text-color" href="#!"><img class="image-size" src="/public/assets/insta.png" alt=""> Very_timothy</a>
                 </li>
               </ul>
 
             </div>
-            <!-- Grid column -->
 
         </div>
-        <!-- Grid row -->
 
       </div>
-      <!-- Footer Links -->
 
       <!-- Copyright -->
-      <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <div class="footer-copyright text-center py-3 footer-text-color credit-height">© 2018 Copyright: alctera graphics design
       </div>
       <!-- Copyright -->
 
