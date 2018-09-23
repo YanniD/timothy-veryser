c00d4276d3d93dffc11705e93404959f3e4c3976
YanniD <yanni.derous@student.howest.be>
2018-09-23 12:47:35 +0200

    video's finished
    video mobile friendly

--- pages/videos.php
+++ pages/videos.php
@@ -1,7 +1,9 @@
 
 <div class="row videos-bg">
-  <div class="col-lg-12 col-xs-0 videos-height">
+  <div class="col-lg-12 col-xs-0 videos-height col-centered">
     <h2 class="text-center">Video's</h2>
+    <div class='container '>
+
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
@@ -9,15 +11,25 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Tu0JdXcPH8M" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  <div class="carousel-inner ">
+
+    <div class="carousel-item active mx-auto ">
+      <div class='iframe-container'>
+          <iframe width="560" height="315"  src="https://www.youtube.com/embed/Tu0JdXcPH8M" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+
     </div>
-    <div class="carousel-item">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/AS96tHgdWLg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <div class="carousel-item mx-auto ">
+      <div class='iframe-container'>
+        <iframe width="560" height="315"  src="https://www.youtube.com/embed/AS96tHgdWLg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+
     </div>
-    <div class="carousel-item">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/u9_rZhJC_gU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <div class="carousel-item mx-auto">
+      <div class='iframe-container'>
+        <iframe width="560" height="315"  src="https://www.youtube.com/embed/u9_rZhJC_gU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -29,5 +41,6 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+  </div>
   </div>
 </div>
